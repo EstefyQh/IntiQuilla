b3960f141d521584ef22e5c55be63df9c175ede4
Estefy <estefany.qh@gmail.com>
2023-05-23 00:34:54 -0300

    Agrego los js

---
+++ js/header.js
@@ -0,0 +1,46 @@
+let miHeader=`
+
+<img src="img/Logo.png" class="logo" alt="IntiQuilla"> 
+
+<div id="meniu">
+
+  <div id="handle">
+  <div class="line_nav"></div>
+  <div class="line_nav"></div>
+  <div class="line_nav"></div>
+  <img src="img/Logo.png" class="logo-nav" alt="IntiQuilla">
+  </div>
+
+<div class="nav">
+<ul>
+  <li><a class="nav-links" href="index.html">INICIO</a></li>
+  <li><a class="nav-links" href="#">NOVEDADES</a></li>
+
+  <li class="dropdown"><a class="nav-links" href="#">LIBROS</a>
+    <ul class="submenu">
+      <li class="sublink"><a class="sublinks" href="Artes.html">Artes</a></li>
+      <li class="sublink"><a class="sublinks" href="Biologia.html">Biologia</a></li>
+      <li class="sublink"><a class="sublinks" href="Cocina.html">Cocina, Comidas y Bebidas</a></li>
+      <li class="sublink"><a class="sublinks" href="Computacion.html">Informatica</a></li>
+      <li class="sublink"><a class="sublinks" href="Comics.html">Comics y Manga</a></li>
+      <li class="sublink"><a class="sublinks" href="Deportes.html">Deportes y Recreación al Aire Libre</a></li>
+      <li class="sublink"><a class="sublinks" href="Economia.html">Economía</a></li>
+      <li class="sublink"><a class="sublinks" href="Salud.html">Salud</a></li>
+      <li class="sublink"><a class="sublinks" href="Ficcion.html">Ficción</a></li>
+      <li class="sublink"><a class="sublinks" href="Literatura.html">Literatura Juvenil</a></li>
+    </ul>
+    <ul>
+        
+    </ul>
+  </li>
+  <li><a class="nav-links" href="contacto.html">CONTACTO</a></li>
+</ul>
+</div>
+</div>
+
+`
+document.querySelector("header").innerHTML=miHeader;
+
+
+
+
